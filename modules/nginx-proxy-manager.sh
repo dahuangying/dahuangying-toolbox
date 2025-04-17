c0928daad0302372e7c3fc6b94f5591726c6d7b4
dahuangying <127608168+dahuangying@users.noreply.github.com>
2025-04-17 22:56:22 +0800

    Update nginx-proxy-manager.sh

--- modules/nginx-proxy-manager.sh
+++ modules/nginx-proxy-manager.sh
@@ -114,7 +114,9 @@ block_ip_port_access() {
 
 # 主菜单
 show_menu() {
-    echo -e "${GREEN}Nginx Proxy Manager 管理菜单${NC}"
+    echo -e "${GREEN}大黄鹰-Linux服务器运维工具箱菜单-Nginx${NC}"
+    echo -e "欢迎使用本脚本，请根据菜单选择操作："
+    echo -e "${GREEN}==============================${NC}"
     echo "1. 安装"
     echo "2. 更新"
     echo "3. 卸载"
