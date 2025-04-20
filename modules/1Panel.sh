5c54076bf52ade01f669d8e51a50b02e41b852ed
dahuangying <127608168+dahuangying@users.noreply.github.com>
2025-04-20 14:26:34 +0800

    Update 1Panel.sh

--- modules/1Panel.sh
+++ modules/1Panel.sh
@@ -15,14 +15,16 @@ show_menu() {
     echo "========================="
     echo "1. 安装 1Panel"
     echo "2. 查看面板信息"
-    echo "3. 卸载 1Panel"
+    echo "3. 修改面板密码"
+    echo "4. 卸载 1Panel"
     echo "0. 退出"
     echo "========================="
     read -p "请输入选项: " option
     case $option in
         1) install_panel ;;
         2) view_panel_info ;;
-        3) uninstall_panel ;;
+        3) change_password ;;
+        4) uninstall_panel ;;
         0) exit 0 ;;
         *) echo "无效的选项，请重新选择！" && sleep 2 && show_menu ;;
     esac
@@ -42,20 +44,33 @@ install_panel() {
 
 # 函数：查看面板信息
 view_panel_info() {
-    echo "查看面板信息..."
+    echo "正在获取面板信息..."
     1pctl user-info
 
-    # 提示用户输入新密码
-echo "请输入新密码: "
-read -sp "新密码: " new_password
-echo
-read -sp "确认新密码: " confirm_password
-echo
+    echo "============================="
+    echo "您可以使用以下命令修改密码："
+    echo "1pctl update password"
+    echo "============================="
+    sleep 2
+    show_menu
+}
 
+# 函数：修改面板密码
+change_password() {
+    echo "开始修改面板密码..."
+    read -sp "请输入新密码: " new_password
+    echo
+    read -sp "请再次输入新密码确认: " confirm_password
+    echo
 
-fi
-    
-
+    # 判断两次密码是否一致
+    if [[ "$new_password" == "$confirm_password" ]]; then
+        # 修改密码
+        echo "$new_password" | 1pctl update password
+        echo "密码修改成功！"
+    else
+        echo "密码不一致，请重新输入。"
+    fi
     sleep 2
     show_menu
 }
@@ -78,6 +93,7 @@ uninstall_panel() {
 
 # 启动脚本
 show_menu
+
 
 
 
