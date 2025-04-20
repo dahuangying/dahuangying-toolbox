4d021b07e401edda9e381ed8f816aa9b99c3c629
dahuangying <127608168+dahuangying@users.noreply.github.com>
2025-04-20 15:34:44 +0800

    Update network.sh

--- modules/network.sh
+++ modules/network.sh
@@ -68,14 +68,14 @@ install_1pane_panel() {
 # 8. NginxProxyManager 可视化面板
 install_nginx_proxy_manager() {
     echo -e "${GREEN}安装 Nginx Proxy Manager 可视化面板...${NC}"
-    # 填入对应的安装命令
+    # bash modules/nginx-proxy-manager.sh
     pause
 }
 
 # 9. AList网盘
 install_alist() {
     echo -e "${GREEN}安装 AList 网盘...${NC}"
-    # 填入对应的安装命令
+    # # bash modules/alist.sh
     pause
 }
 
@@ -142,10 +142,10 @@ show_menu() {
             install_1pane_panel
             ;;
         8)
-            bash modules/nginx-proxy-manager.sh
+            install_nginx_proxy_manager
             ;;
         9)
-            bash modules/alist.sh
+            install_alist
             ;;
         10)
             install_oracle_keep_alive
